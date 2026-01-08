4a6943d9cb9bad2c05b10fa10d5b7885cabafe2a
juspietrasik5-lab <juspietrasik5@gmail.com>
2026-01-08 13:22:09 +0100

    Agregaty2.sql

---
+++ Agregaty2.sql
@@ -0,0 +1,6 @@
+SELECT
+  ROUND(AVG(age), 2) AS srednia_wieku,
+  ABS(-3)            AS wartosc_bezwzgledna,
+  2 * 3              AS mnozenie,
+  6 / 2              AS dzielenie
+FROM aggregates.batman;
